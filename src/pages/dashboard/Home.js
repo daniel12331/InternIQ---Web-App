475493ab7ffedb909076ce0f62b5b23909dd84ff
Daniel Marko <20088708@mail.wit.ie>
2023-03-21 10:45:34 +0000

    Clean up

--- src/pages/dashboard/Home.js
+++ src/pages/dashboard/Home.js
@@ -2,7 +2,15 @@ import { useEffect } from 'react';
 import { StatsContainer, Loading, ChartsContainer } from '../../components';
 import { useDispatch, useSelector } from 'react-redux';
 import { showStats } from '../../features/allJobs/allJobSlice';
+import { Document, Page, pdfjs} from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+const url = 
+"https://cdn.filestackcontent.com/wcrjf9qPTCKXV3hMXDwK"
+  
 const Home = () => {
   const { isLoading, monthlyApplications } = useSelector(
     (store) => store.allJobs
@@ -10,11 +18,16 @@ const Home = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(showStats());
+    //dispatch(showStats());
   }, []);
 
   return (
     <>
+      <Document
+        file={url}
+      >
+        <Page pageNumber={1} />
+      </Document>
       <StatsContainer />
       {monthlyApplications.length > 0 && <ChartsContainer />}
     </>
